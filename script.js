066b708ddbf8c2c7f62e439c8c80432c5c436948
codingcodymiller <cody.miller@learningfuze.com>
2018-08-23 16:37:47 -0700

    added event handlers, added function placeholders

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
 $('document').ready(initialize);
 
 var $gameBoard;
+var currentBoardSize;
 
 //calls all function necessary to start gameplay
 function initialize(){
     createReferenceToDomElements();
+    applyEventHandlers();
+}
+
+function applyEventHandlers(){
+    $("#game-area").on("click", ".clickable", placePiece);
+    $(".board-size-3").on("click", ()=>{
+        reset(3);
+    })
+    $(".board-size-4").on("click", ()=>{
+        reset(4);
+    })
+    $(".board-size-5").on("click", ()=>{
+        reset(5);
+    })
+    $(".reset").on("click", ()=>{
+        reset(currentBoardSize);
+    })
 }
 
 function createReferenceToDomElements(){
     $gameBoard = $('#game-area');
+}
+
+function updateStats(){
+
+}
+
+function updateModal(){
+
+}
+
+function showModal(){
+
+}
+
+function clearBoard(){
+
+}
+
+function placePiece(){
+
+}
+
+function reset(boardSize){
+    clearBoard();
+    generateBoardArray(boardSize);
+    generateGameBoard(boardArray);
+}
+
+function togglePlayer(){
+
 }
