e6dc7886edf6374e758ef5d7469919386b5a4fab
zhiwutian <brettalbright@outlook.com>
2018-08-23 16:43:14 -0700

    completed show/hide modal function

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ var $gameBoard;
 
 //calls all function necessary to start gameplay
 function initialize(){
+    $("#win-modal").addClass("hide");
     createReferenceToDomElements();
 }
 
 function createReferenceToDomElements(){
     $gameBoard = $('#game-area');
+}
+
+
+function modalShowHide () {
+    if ($("#win-modal").hasClass("hide")) {
+        $("#win-modal").removeClass("hide");
+    } else {
+        $("#win-modal").addClass("hide");
+    }
+
 }
